1c7af5254c17b13e533bd2032d204c123930d57f
TinoIf <tinoinformatics@gmail.com>
2025-11-17 04:56:03 +0700

    Add GlobalState management for checkpoint position and fire count

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 func _on_play_pressed() -> void:
+	GlobalState.last_checkpoint_position = Vector2.ZERO
+	GlobalState.current_fire = 0
 	get_tree().change_scene_to_file("res://scene/world.tscn")
 
 
